e3bcc95236b23de9060aaeae41761410ebf829ce
Kuan0143 <houseofcards0413@gmail.com>
2018-05-26 19:28:10 +0800

    一顆球

--- main.js
+++ main.js
@@ -21,3 +21,15 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
 }
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+
